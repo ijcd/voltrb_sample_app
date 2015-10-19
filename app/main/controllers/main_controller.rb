899eaff178bd13e319b5c6b7d8818b272ee11dee
Ian Duggan <ian@ianduggan.net>
2015-10-18 19:04:52 -0700

    Sample app w/ mongo issue.

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 # By default Volt generates this controller for your Main component
 module Main
   class MainController < Volt::ModelController
+    model :store
+
     def index
       # Add code for when the index view is loaded
     end
@@ -9,19 +11,54 @@ module Main
       # Add code for when the about view is loaded
     end
 
-    private
-
-    # The main template contains a #template binding that shows another
-    # template.  This is the path to that template.  It may change based
-    # on the params._component, params._controller, and params._action values.
-    def main_path
-      "#{params._component || 'main'}/#{params._controller || 'main'}/#{params._action || 'index'}"
+    def add_todo
+      _todos << { name: page._new_todo }
+      page._new_todo = ''
     end
 
-    # Determine if the current nav component is the active one by looking
-    # at the first part of the url against the href attribute.
-    def active_tab?
-      url.path.split('/')[1] == attrs.href.split('/')[1]
+    def current_todo
+      _todos[(params._index || 0).to_i]
     end
+
+    def check_all
+      _todos.each { |todo| todo._completed = true }
+    end
+
+    def completed
+      _todos.count { |t| t._completed }
+    end
+
+    def incomplete
+      # because .size and completed both return promises, we need to
+      # call .then on them to get their value.
+      _todos.size.then do |size|
+        completed.then do |completed|
+          size - completed
+        end
+      end
+    end
+
+    def percent_complete
+      _todos.size.then do |size|
+        completed.then do |completed|
+          (completed / size.to_f * 100).round
+        end
+      end
+    end
+
+private
+
+# The main template contains a #template binding that shows another
+# template.  This is the path to that template.  It may change based
+# on the params._component, params._controller, and params._action values.
+def main_path
+  "#{params._component || 'main'}/#{params._controller || 'main'}/#{params._action || 'index'}"
+end
+
+# Determine if the current nav component is the active one by looking
+# at the first part of the url against the href attribute.
+def active_tab?
+  url.path.split('/')[1] == attrs.href.split('/')[1]
+end
   end
 end
